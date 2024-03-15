41f633b1b356945ce78394940a696f0d55ff376c
experi-do <goodijdh@naver.com>
2024-03-16 01:59:18 +0900

    Feat: 2812 솔루션

--- Week3 정도현/Greedy Algorithm/2812.py
+++ Week3 정도현/Greedy Algorithm/2812.py
@@ -4,8 +4,21 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split(' '))
-num = list(map(int, input().split(' ')))
+num = input()
+num = [int(num[i]) for i in range(n)]
+stack = [num[0]]
 
-result=[0 for _ in range(n-k)]
-cnt = 0
-for i in range(num):
+for i in range(1, len(num)):
+
+    while len(stack) > 0 and k > 0:
+        if stack[-1] < num[i]:
+            stack.pop()
+            k -= 1
+        else:
+            break
+    stack.append(num[i])
+
+if k > 0:
+    print(''.join(map(str, stack[:-k])))
+else:
+    print(''.join(map(str, stack)))
