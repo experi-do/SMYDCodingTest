a777ddfdb7779548995aa246d616702c0b9157c0
Jung DoHyun <64680042+experi-do@users.noreply.github.com>
2025-04-11 04:55:42 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,32 @@
+'''
+    처음부터 끝까지 이동할 때 지나야 하는 노드 수를 구하는 문제
+'''
+
 import sys
-from collections import deque
+from collections import defaultdict, deque
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+
 grid = [list(map(int, input().strip())) for _ in range(n)]
 visited = [[False for _ in range(m)] for _ in range(n)]
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
 
-queue = deque([(0, 0)])  # Start with the top-left corner
+dx = [-1, 0, 1 ,0]
+dy = [0, 1, 0, -1]
+
+queue = deque()
+queue.append([0, 0])
 visited[0][0] = True
-cnt = 0
-
-# Perform BFS
 while queue:
-    cnt += 1
-    for _ in range(len(queue)):
-        y, x = queue.popleft()
+    y, x = queue.popleft()
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
 
-        # Check if we've reached the bottom-right corner
-        if y == n - 1 and x == m - 1:
-            print(cnt)
-            sys.exit()
+        if 0 <= ny < n and 0 <= nx < m and grid[ny][nx] == 1 and visited[ny][nx] == False:
+            visited[ny][nx] = True
+            queue.append([ny, nx])
+            grid[ny][nx] = grid[y][x] + 1
 
-        # Explore the 4 possible directions
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-
-            if 0 <= ny < n and 0 <= nx < m and grid[ny][nx] == 1 and not visited[ny][nx]:
-                visited[ny][nx] = True
-                queue.append((ny, nx))
-
-# If no path is found to the bottom-right corner
-print(-1)
+print(grid[n-1][m-1])
