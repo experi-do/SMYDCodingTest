0bc589a2ca8a2e8c635b0cfcf11fde20513fb6a8
Jung DoHyun <64680042+experi-do@users.noreply.github.com>
2025-07-17 21:07:14 +0900

    [Gold IV] Title: 연구소, Time: 1556 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,56 +1,69 @@
-import sys
-input = sys.stdin.readline
+import sys, math
+from itertools import combinations
 
-import copy
+'''
+세균 dfs -> max count == min safety zone
+'''
 
-from collections import deque
+N, M = map(int, sys.stdin.readline().split())
 
-def bfs():
-    global answer
+grid = []
+for _ in range(N):
+    grid.append(list(map(int, sys.stdin.readline().split())))
 
-    q = deque()
-    safe = copy.deepcopy(map_list)
+blank = []
+virus = []
+wall = []
+for i in range(N):
+    for j in range(M):
+        if grid[i][j] == 0:
+            blank.append((i, j))
+        elif grid[i][j] == 2:
+            virus.append((i, j))
+        else:
+            wall.append((i, j))
 
-    for i in range(n):
-        for j in range(m):
-            if safe[i][j] == 2:
-                q.append([i, j])
+blank_comb = combinations(blank, 3)
 
-    while q:
-        x, y = q.popleft()
+def dfs():
 
-        dx = [0, 0, -1, 1]
-        dy = [-1, 1, 0, 0]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    visited = [[False] * M for _ in range(N)]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-            if (0 <= nx < n) and (0 <= ny < m):
-                if safe[nx][ny] == 0:
-                    safe[nx][ny] = 2
-                    q.append([nx, ny])
+    virus_zone = 0
+    for v in virus:
+        x, y = v
+        stack =[(x, y)]
+        visited[x][y] = True
+        cnt = 0
+        while stack:
+            cx, cy = stack.pop()
+            for i in range(4):
+                nx = cx + dx[i]
+                ny = cy + dy[i]
+                if (0 <= nx < N) and (0 <= ny < M) and visited[nx][ny] == False and grid[nx][ny] == 0:
+                    stack.append((nx, ny))
+                    visited[nx][ny] = True
+                    cnt += 1
+        virus_zone += cnt
+    return virus_zone
 
-    temp = 0
-    for i in range(n):
-        temp += safe[i].count(0)
 
-    answer = max(answer, temp)
 
-def wall(count):
-    if count == 3:
-        bfs()
-        return
+answer = N*M
+for comb in blank_comb:
+    # 벽 3개 추가
+    new_grid = grid.copy()
+    for node in comb:
+        x, y = node
+        grid[x][y] = 1
+    virus_zone = dfs()
+    answer = min(answer, virus_zone)
+    for node in comb:
+        x, y = node
+        grid[x][y] = 0
 
-    for i in range(n):
-        for j in range(m):
-            if map_list[i][j] == 0:
-                map_list[i][j] = 1
-                wall(count+1)
-                map_list[i][j] = 0
-
-n, m = map(int, input().split())
-answer = 0
-map_list = [list(map(int,input().split())) for _ in range(n)]
-
-wall(0)
-print(answer)
+wall_node = len(wall) + 3
+virus_node = len(virus) + answer
+print(N*M - (wall_node + virus_node))
