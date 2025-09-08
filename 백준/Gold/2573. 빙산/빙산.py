03c1f07556b9e61d67439c64ef9e7979536f169d
Jung DoHyun <64680042+experi-do@users.noreply.github.com>
2025-09-08 23:10:24 +0900

    [Gold IV] Title: 빙산, Time: 768 ms, Memory: 196148 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -9,6 +9,7 @@ for _ in range(n):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+
 def countIce():
     visited = [[False for _ in range(m)] for _ in range(n)]
     cnt = 0
@@ -27,6 +28,7 @@ def countIce():
                             visited[nx][ny] = True
                             q.append((nx, ny))
     return cnt
+
 
 answer = 0
 while True:
@@ -50,7 +52,7 @@ while True:
                     if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 0:
                         water_cnt += 1
                 melt_amounts[i][j] = water_cnt
-
+                
     for i in range(n):
         for j in range(m):
             if grid[i][j] > 0:
